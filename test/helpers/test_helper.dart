b7c52a7819cc5db1a2b7495c30f903461b86b683
João Paulo Camargo <joao.camargo@willbank.com.br>
2023-10-27 10:59:27 -0300

    test: create mock for get current weather usecase

--- test/helpers/test_helper.dart
+++ test/helpers/test_helper.dart
@@ -2,11 +2,13 @@ import 'package:http/http.dart' as http;
 import 'package:mockito/annotations.dart';
 import 'package:tdd_weather_app/data/data_sources/weather_remote_data_source.dart';
 import 'package:tdd_weather_app/domain/repositories/weather_repository.dart';
+import 'package:tdd_weather_app/domain/usecases/get_current_weather.dart';
 
 @GenerateMocks(
   [
     WeatherRepository,
     WeatherRemoteDataSource,
+    GetCurrentWeatherUseCase,
   ],
   customMocks: [MockSpec<http.Client>(as: #MockHttpClient)],
 )
